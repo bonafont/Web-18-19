64821b6b10415512c742085d081d48e2c60d826d
CarloneLover <anthonyfey@gmail.com>
2018-09-13 14:18:10 +0200

    VAGINA

--- Site/index.php
+++ Site/index.php
@@ -42,6 +42,7 @@ require 'pages/data/Donnees.inc.php';
       exit(-1);
       }
       else{
+        $conn->query("SET NAMES UTF8");
         /*
 
           CREATION DE LA TABLE COCKTAIL
@@ -76,7 +77,7 @@ require 'pages/data/Donnees.inc.php';
           foreach($cocktail['index'] as $index){
             $titre = $conn->real_escape_string($cocktail['titre']);
             $ingredients = $conn->real_escape_string($index);
-            $sql = "INSERT INTO Cocktail (titre, recette , ingredients, preparation) VALUES ('" .$titre . "','". $recette."','". $ingredients."','".$preparation . "')";
+            $sql = "INSERT INTO Cocktail (titre, recette , ingredients, preparation) VALUES ('" .utf8_encode($titre) . "','". $recette."','". $ingredients."','".$preparation . "')";
             if (!($conn->query($sql) === TRUE)) {
               //ERREUR INSERTION DANS LA BDD
               echo "Error: " . $sql . "<br>" . $conn->error;
